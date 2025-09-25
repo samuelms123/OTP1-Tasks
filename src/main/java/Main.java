90ff691563e992e2b6ac17825f6e64f4e38e607c
Samuelms <samuel.sarimo@gmail.com>
2025-09-25 12:12:13 +0300

    Modify main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@ import Tasks.TemperatureConverter;
 public class Main {
     public static void main(String[] args) {
         TemperatureConverter converter = new TemperatureConverter();
-        System.out.println("Result: " + converter.fahrenheitToCelsius(32));
+        System.out.println("100 fahrenheit to celsius");
+        System.out.println("Result: " + converter.fahrenheitToCelsius(100));
 
     }
 }
