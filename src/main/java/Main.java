1fd12475820ebe9d7f264b9159d97d8b14ce1e57
Samuelms <samuel.sarimo@gmail.com>
2025-09-25 11:57:53 +0300

    Add main

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import Tasks.TemperatureConverter;
+
+public class Main {
+    public static void main(String[] args) {
+        TemperatureConverter converter = new TemperatureConverter();
+        System.out.println("Result: " + converter.fahrenheitToCelsius(32));
+
+    }
+}
